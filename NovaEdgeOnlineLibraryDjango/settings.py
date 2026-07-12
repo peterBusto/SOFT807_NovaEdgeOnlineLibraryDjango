b85381dbbe8840de9802098f399423e87346fba2
Peter <pebu001@ess.ais.ac.nz>
2026-07-13 00:18:21 +1200

    Updated the db connecting to ms azure

--- NovaEdgeOnlineLibraryDjango/settings.py
+++ NovaEdgeOnlineLibraryDjango/settings.py
@@ -77,7 +77,7 @@ WSGI_APPLICATION = 'NovaEdgeOnlineLibraryDjango.wsgi.application'
 
 
 # Database
-# https://docs.djangoproject.com/en/6.0/ref/settings/#databases
+# https://docs.djangoproject.com/en/6.0/ref/settings/#databases.
 
 DATABASES = {
     'default': {
