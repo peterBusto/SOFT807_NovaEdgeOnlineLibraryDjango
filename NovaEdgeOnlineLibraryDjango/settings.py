4016506f6269d4e20711c44ee50b5faf62e10d5a
Regin <regin.garcia.balangatan@gmail.com>
2026-08-03 16:56:34 +1200

    JWT authentication update

--- NovaEdgeOnlineLibraryDjango/settings.py
+++ NovaEdgeOnlineLibraryDjango/settings.py
@@ -149,6 +149,8 @@ STATIC_ROOT = BASE_DIR / 'staticfiles'
 AUTH_USER_MODEL = 'users.User'
 
 # REST Framework Configuration
+# REST Framework Configuration update
+
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': [
         'rest_framework.authentication.TokenAuthentication',
